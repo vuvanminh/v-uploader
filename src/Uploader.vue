4e863813e4bb7ae8d77fb633ad76bfa33ced9a53
minhvv <minhvv@wiserobot.com>
2018-09-30 10:05:36 +0700

    Update Class Support Foundation

--- src/Uploader.vue
+++ src/Uploader.vue
@@ -25,40 +25,42 @@
                     aria-live="polite"
                     aria-relevant="additions removals">
                     <li class="columns small-12 medium-6 large-6">
-                        <span role="status" class="qq-upload-status-text-selector qq-upload-status-text"></span>
-                        <div class="qq-progress-bar-container-selector qq-progress-bar-container">
-                            <div role="progressbar"
-                                 aria-valuenow="0"
-                                 aria-valuemin="0"
-                                 aria-valuemax="100"
-                                 class="qq-progress-bar-selector qq-progress-bar"></div>
-                        </div>
-                        <span class="qq-upload-spinner-selector qq-upload-spinner"></span>
-                        <div class="qq-thumbnail-wrapper">
-                            <img class="qq-thumbnail-selector" qq-max-size="120" qq-server-scale>
-                        </div>
-                        <button type="button" class="qq-upload-cancel-selector qq-upload-cancel">X</button>
-                        <button type="button" class="qq-upload-retry-selector qq-upload-retry">
-                            <span class="qq-btn qq-retry-icon" aria-label="Retry"></span> Retry
-                        </button>
-
-                        <div class="qq-file-info">
-                            <div class="qq-file-name">
-                                <span class="qq-upload-file-selector qq-upload-file"></span>
-                                <span class="qq-edit-filename-icon-selector qq-edit-filename-icon"
-                                      aria-label="Edit filename"></span>
+                        <div class="inner">
+                            <span role="status" class="qq-upload-status-text-selector qq-upload-status-text"></span>
+                            <div class="qq-progress-bar-container-selector qq-progress-bar-container">
+                                <div role="progressbar"
+                                     aria-valuenow="0"
+                                     aria-valuemin="0"
+                                     aria-valuemax="100"
+                                     class="qq-progress-bar-selector qq-progress-bar"></div>
                             </div>
-                            <input class="qq-edit-filename-selector qq-edit-filename" tabindex="0" type="text">
-                            <span class="qq-upload-size-selector qq-upload-size"></span>
-                            <button type="button" class="qq-btn qq-upload-delete-selector qq-upload-delete">
-                                <span class="qq-btn qq-delete-icon" aria-label="Delete"></span>
+                            <span class="qq-upload-spinner-selector qq-upload-spinner"></span>
+                            <div class="qq-thumbnail-wrapper">
+                                <img class="qq-thumbnail-selector" qq-max-size="120" qq-server-scale>
+                            </div>
+                            <button type="button" class="qq-upload-cancel-selector qq-upload-cancel">X</button>
+                            <button type="button" class="qq-upload-retry-selector qq-upload-retry">
+                                <span class="qq-btn qq-retry-icon" aria-label="Retry"></span> Retry
                             </button>
-                            <button type="button" class="qq-btn qq-upload-pause-selector qq-upload-pause">
-                                <span class="qq-btn qq-pause-icon" aria-label="Pause"></span>
-                            </button>
-                            <button type="button" class="qq-btn qq-upload-continue-selector qq-upload-continue">
-                                <span class="qq-btn qq-continue-icon" aria-label="Continue"></span>
-                            </button>
+
+                            <div class="qq-file-info">
+                                <div class="qq-file-name">
+                                    <span class="qq-upload-file-selector qq-upload-file"></span>
+                                    <span class="qq-edit-filename-icon-selector qq-edit-filename-icon"
+                                          aria-label="Edit filename"></span>
+                                </div>
+                                <input class="qq-edit-filename-selector qq-edit-filename" tabindex="0" type="text">
+                                <span class="qq-upload-size-selector qq-upload-size"></span>
+                                <button type="button" class="qq-btn qq-upload-delete-selector qq-upload-delete">
+                                    <span class="qq-btn qq-delete-icon" aria-label="Delete"></span>
+                                </button>
+                                <button type="button" class="qq-btn qq-upload-pause-selector qq-upload-pause">
+                                    <span class="qq-btn qq-pause-icon" aria-label="Pause"></span>
+                                </button>
+                                <button type="button" class="qq-btn qq-upload-continue-selector qq-upload-continue">
+                                    <span class="qq-btn qq-continue-icon" aria-label="Continue"></span>
+                                </button>
+                            </div>
                         </div>
                     </li>
                 </ul>
